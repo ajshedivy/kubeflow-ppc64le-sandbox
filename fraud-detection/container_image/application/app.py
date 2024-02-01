9386e429a20daff1ad7303b709c6322b123f6ba3
ajshedivy <ajshedivyaj@gmail.com>
2024-02-01 12:03:37 -0600

    add fraud detection app

---
+++ fraud-detection/container_image/application/app.py
@@ -0,0 +1,55 @@
+import tornado.ioloop
+import tornado.web
+import tornado.websocket
+import json
+import pandas as pd
+from trino.dbapi import Connection
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("index.html")
+
+class WebSocketHandler(tornado.websocket.WebSocketHandler):
+    def open(self):
+        print("WebSocket opened")
+
+    def on_message(self, message):
+        try:
+            number_of_rows = int(message)
+            # Simulating a database query. Replace with your actual database query logic.
+            data = query_database(number_of_rows)
+            # Convert your query result to JSON format and send it to the client
+            self.write_message(json.dumps(data))
+        except ValueError:
+            self.write_message("Please enter a valid integer.")
+
+    def on_close(self):
+        print("WebSocket closed")
+
+def query_database(number_of_rows):
+    # result = [{"id": i, "data": f"data {i}"} for i in range(number_of_rows)]
+    # return result
+    with Connection(
+        host="trino.trino",
+        port="8080",
+        user="anybody",
+        catalog="jtopen",
+        schema="demo",
+    ) as conn:
+        link = conn.cursor()
+        link.execute(f"SELECT * FROM fraud LIMIT {number_of_rows} OFFSET {1_000_000}")
+        return pd.DataFrame(link.fetchall(), columns=[i.name for i in link.description]).to_dict(orient='index')
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+        (r"/websocket", WebSocketHandler),
+    ],
+    template_path='templates',
+    static_path='static')
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
+
