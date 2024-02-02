6fb36a84d654a4bfe911c2507b761ea2a1121a39
ajshedivy <ajshedivyaj@gmail.com>
2024-02-01 18:37:13 -0600

    add back trino client

--- fraud-detection/container_image/application/app.py
+++ fraud-detection/container_image/application/app.py
@@ -6,13 +6,15 @@ import pandas as pd
 from trino.dbapi import Connection
 import logging
 
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
 
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("""
+        self.write(
+            """
         <html>
         <body>
         <form action="/data" method="post">
@@ -21,44 +23,49 @@ class MainHandler(tornado.web.RequestHandler):
         </form>
         </body>
         </html>
-        """)
+        """
+        )
+
 
 class DBHandler(tornado.web.RequestHandler):
     def post(self):
         num_rows = int(self.get_body_argument("num_rows"))
         data = query_database(num_rows)
-        json_data = data.to_dict(orient='records')
+        json_data = data.to_dict(orient="records")
         self.write(f"data: {json_data}")
 
 
 def query_database(number_of_rows):
     try:
-        logging.info('Establishing connection to Trino')
-        # with Connection(
-        #     host="trino.trino",
-        #     port="8080",
-        #     user="anybody",
-        #     catalog="jtopen",
-        #     schema="demo",
-        # ) as conn:
-        #     link = conn.cursor()
-        #     link.execute(f"SELECT * FROM fraud LIMIT {number_of_rows} OFFSET {1_000_000}")
-        #     return pd.DataFrame(
-        #         link.fetchall(), 
-        #         columns=[i.name for i in link.description])
-        df = pd.DataFrame([[1,2,3], [4,5,6]])
-        return df
+        logging.info("Establishing connection to Trino")
+        with Connection(
+            host="trino.trino",
+            port="8080",
+            user="anybody",
+            catalog="jtopen",
+            schema="demo",
+        ) as conn:
+            link = conn.cursor()
+            link.execute(
+                f"SELECT * FROM fraud LIMIT {number_of_rows} OFFSET {1_000_000}"
+            )
+            return pd.DataFrame(
+                link.fetchall(), columns=[i.name for i in link.description]
+            )
     except Exception as e:
-        logging.info(f'Exception occcured with trino: {e}')
+        logging.info(f"Exception occcured with trino: {e}")
+
 
 def make_app():
-    return tornado.web.Application([
-        (r"/", MainHandler),
-        (r"/data", DBHandler),
-    ])
+    return tornado.web.Application(
+        [
+            (r"/", MainHandler),
+            (r"/data", DBHandler),
+        ]
+    )
+
 
 if __name__ == "__main__":
     app = make_app()
     app.listen(8888)
     tornado.ioloop.IOLoop.current().start()
-
